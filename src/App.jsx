0ae6f4bdf0f6ec0db24ed4b1b6db1bcccf01fc0e
Rajat <rajat804@gmail.com>
2026-05-06 18:43:19 +0530

    app jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import WebLayout from "./components/weboutlet/WebLayout"
+import AboutUs from "./pages/Aboutus"
+import FAQ from "./pages/Faq"
+import Gallery from "./pages/Gallery"
+import Attorneys from "./pages/Attorneys"
+import Internship from "./pages/Internship"
+import AdvisoryMembers from "./pages/AdvisoryMembers"
+import BlogPage from "./pages/BlogPage"
+import Contact from "./pages/Contact"
 
 const App = () => {
   return (
@@ -10,6 +18,14 @@ const App = () => {
       <Routes>
         <Route path="/" element={<WebLayout />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<AboutUs/>} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/attorney" element={<Attorneys />} />
+          <Route path="/internship" element={<Internship />} />
+          <Route path="/members" element={<AdvisoryMembers />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </Router>
