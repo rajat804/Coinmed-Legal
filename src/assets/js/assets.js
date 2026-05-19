93614e95ca2ac802b1dc016c35cb74a93bd2c709
Rajat <rajat804@gmail.com>
2026-05-19 12:40:35 +0530

    assets update

--- src/assets/js/assets.js
+++ src/assets/js/assets.js
@@ -23,8 +23,35 @@ import service11 from '../img/service-11.png';
 import service12 from '../img/service-12.png';
 import service13 from '../img/service-13.png';
 
+// logo
+import logo from '../img/logo.jpg';
+import removeLogo from '../img/logo-removebg-preview.png';
+
+// gallery court room tab
+import courtroom1 from '../img/court-room-1.jpg';
+import courtroom2 from '../img/court-room-2.png';
+import courtroom3 from '../img/court-room-3.jpg';
+import courtroom4 from '../img/court-room-4.jpg';
+
+// lawyers at work tab
+import gallery1 from '../img/gallery-1.jpg';
+import gallery2 from '../img/gallery-2.jpg';
+import gallery3 from '../img/gallery-3.jpg';
+import gallery4 from '../img/gallery-4.jpg';
+
+// miami tab
+import miami1 from '../img/miami-1.jpg';
+import miami2 from '../img/miami-2.jpg';
+import miami3 from '../img/miami.jpg';
+
+
 // Export all assets as an object
 export const assets = {
+
+  // logo images
+  logo,
+  removeLogo,
+
   // Attorney Images
   attorney14,
   attorney15,
@@ -47,6 +74,10 @@ export const assets = {
   service11,
   service12,
   service13,
+
+  // gallery page images
+  courtroom1,
+  courtroom2,courtroom3,courtroom4,gallery1,gallery2,gallery3,gallery4,miami1,miami2,miami3,
 };
 
 // Optional: Export as array if needed
